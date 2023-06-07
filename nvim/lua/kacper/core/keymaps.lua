bb2fe92ca1440f88eb05c895bc574bca413cb875
kacperjarocki <kacper.jarocki1121@gmail.com>
2023-06-07 13:34:30 +0200

    tiling window manager basic setup

--- nvim/lua/kacper/core/keymaps.lua
+++ nvim/lua/kacper/core/keymaps.lua
@@ -4,19 +4,19 @@ local keymap = vim.keymap
 
 --general
 
-keymap.set("n","<leader>nh",":nohl<CR>")
+keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 --windows
-keymap.set("n","<leader>sw","<C-w>v")
-keymap.set("n","<leader>sx",":close<CR>")
-keymap.set("n","<leader>sm",":MaximizerToggle<CR>")
+keymap.set("n", "<leader>sw", "<C-w>v")
+keymap.set("n", "<leader>sx", ":close<CR>")
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
 --tabs
-keymap.set("n", "<leader>to", ":tabnew<CR>") 
+keymap.set("n", "<leader>to", ":tabnew<CR>")
 keymap.set("n", "<leader>tx", ":tabclose<CR>")
 keymap.set("n", "<leader>tn", ":tabn<CR>")
-keymap.set("n", "<leader>tp", ":tabp<CR>") 
+keymap.set("n", "<leader>tp", ":tabp<CR>")
 --File exp
-keymap.set("n","<leader>e",":NvimTreeToggle<CR>")
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 
 --telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
@@ -25,8 +25,8 @@ keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string u
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
 
--- telescope git commands 
+-- telescope git commands
 keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
 keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
 keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
-keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- 
+keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") --
